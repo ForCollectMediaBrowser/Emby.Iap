12fcf2084407f13b851de2a240f8ebf2b79c25bc
Eric Reed <ebr@mediabrowser3.com>
2015-11-03 16:38:57 -0500

    Update for new package and change atv sub sku

--- amazoniap/src/main/java/tv/emby/iap/InAppProduct.java
+++ amazoniap/src/main/java/tv/emby/iap/InAppProduct.java
@@ -25,7 +25,8 @@ public class InAppProduct {
     private static HashMap<String,String> MonthlySubscriptionSkus = new HashMap<>();
     static {
         MonthlySubscriptionSkus.put("com.mb.android","emby.supporter.monthly");
-        MonthlySubscriptionSkus.put("tv.emby.embyatv","emby.supporter.atv.monthly");
+        MonthlySubscriptionSkus.put("tv.emby.embyatv","emby.premiere.atv.monthly");
+        MonthlySubscriptionSkus.put("com.emby.mobile","emby.premiere.monthly");
     }
     private static HashMap<String,String> WeeklySubscriptionSkus = new HashMap<>();
     static {
@@ -36,11 +37,13 @@ public class InAppProduct {
     static {
         LifetimeSubscriptionSkus.put("com.mb.android","emby.supporter.lifetime");
         LifetimeSubscriptionSkus.put("tv.emby.embyatv","emby.supporter.atv.lifetime");
+        LifetimeSubscriptionSkus.put("com.emby.mobile","emby.premiere.lifetime");
     }
     private static HashMap<String,String> UnlockSkus = new HashMap<>();
     static {
         UnlockSkus.put("com.mb.android","com.mb.android.unlock");
         UnlockSkus.put("tv.emby.embyatv","tv.emby.embyatv.unlock");
+        UnlockSkus.put("com.emby.mobile","com.emby.mobile.unlock");
     }
 
     public static String getCurrentMonthlySku(String app) { return MonthlySubscriptionSkus.get(app); }
@@ -52,7 +55,6 @@ public class InAppProduct {
         HashSet<String> skus = new HashSet<>();
         skus.add(getCurrentUnlockSku(app));
         skus.add(getCurrentMonthlySku(app));
-        skus.add(getCurrentWeeklySku(app));
         skus.add(getCurrentLifetimeSku(app));
 
         return skus;
